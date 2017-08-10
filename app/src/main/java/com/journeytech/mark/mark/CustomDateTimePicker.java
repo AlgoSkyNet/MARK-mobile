42b2f7c2092cdf27f8268dfc81a0bde542e1087a
rtyJa <rtyJay14@live.com>
2017-08-10 17:56:49 +0800

    Fix custom date time on some devices

--- app/src/main/java/com/journeytech/mark/mark/CustomDateTimePicker.java
+++ app/src/main/java/com/journeytech/mark/mark/CustomDateTimePicker.java
@@ -120,17 +120,28 @@ public class CustomDateTimePicker implements View.OnClickListener {
         LinearLayout linear_bottom = new LinearLayout(activity);
         linear_match_wrap.topMargin = 8;
         linear_bottom.setLayoutParams(linear_match_wrap);
+        linear_bottom.setGravity(Gravity.CENTER);
 
         btn_set = new Button(activity);
         btn_set.setLayoutParams(button_params);
         btn_set.setText("Set");
         btn_set.setId(SET);
+        btn_set.setHeight(12);
+        LinearLayout.LayoutParams set = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        set.setMargins(0, 0, 0, 5);
+        btn_set.setLayoutParams(set);
         btn_set.setOnClickListener(this);
 
         btn_cancel = new Button(activity);
         btn_cancel.setLayoutParams(button_params);
         btn_cancel.setText("Cancel");
         btn_cancel.setId(CANCEL);
+        btn_cancel.setHeight(12);
+        LinearLayout.LayoutParams cancel = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        cancel.setMargins(0, 0, 0, 5);
+        btn_cancel.setLayoutParams(cancel);
         btn_cancel.setOnClickListener(this);
 
         linear_bottom.addView(btn_set);
